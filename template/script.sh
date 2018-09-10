4085c6d911f43e0fd4e5d85f9469f7482deb117e
chnyda <ced.hnyda@gmail.com>
2018-09-10 15:21:51 +0200

    Add key_file arg

--- template/script.sh
+++ template/script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 {% for node_name, node_ip in nodes %}
-{% if script.user is defined %}
-scp -o "StrictHostKeyChecking no" join-{{ node_name }}.sh {{ script.user }}@{{ node_ip }}:/home/{{ script.user }}
+{% if script is defined %}
+scp -i {{ script.key_file }} -o "StrictHostKeyChecking no" join-{{ node_name }}.sh {{ script.user }}@{{ node_ip }}:/home/{{ script.user }}
 {% else %}
 scp -o "StrictHostKeyChecking no" join-{{ node_name }}.sh {{ node_ip }}:/home/root
 {% endif %}
